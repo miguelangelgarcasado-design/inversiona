dd1f347a86f3d67ec0045cc6c408acbbcbb5081d
miguelangelgarcasado-design <miguelangelgarcasado@gmail.com>
2026-09-10 10:22:50 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,51 @@
-const CACHE_NAME = "inversionia-cache-v2";
+const CACHE_NAME = "inversionia-cache-v3";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(names =>
+        Promise.all(
+          names
+            .filter(name => name !== CACHE_NAME)
+            .map(name => caches.delete(name))
+        )
+      )
+      .then(() => self.clients.claim())
   );
 });
+
+self.addEventListener("fetch", event => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match("./"))
+    );
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copia = response.clone();
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, copia));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
+
