e5e9ade79776794998261f4b1e705295e8b5f6d6
miguelangelgarcasado-design <miguelangelgarcasado@gmail.com>
2026-09-17 13:59:23 +0200

    Update news.js

--- functions/api/news.js
+++ functions/api/news.js
@@ -12,7 +12,7 @@ async function procesarTraducciones() {
 
   try {
     const res = await fetch(
-    "https://translate.googleapis.com/translate_a/t?client=gtx&sl=en&tl=es&dt=t",
+   "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=es&dt=t",
       {
         method: "POST",
         headers: {
